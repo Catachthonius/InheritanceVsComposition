a760c4982bdc0ccf429a8afc3b1a333a0eda34f0
Antonio <catachthonius01@gmail.com>
2023-07-09 01:08:19 +0200

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+//Write two different implementations of 1 parent and 2 children classes that override one method:
+// one using inheritance and another using interfaces.
+
+open class Parent {
+    private var parentHeightInCm = 177
+    private var parentWeightInKg = 89
+    open fun printVariables() {
+        println("Registered parent height is: $parentHeightInCm \n Registered parent weight is: $parentWeightInKg")
+    }
+}
+
+interface Hair {
+    fun hairColor()
+}
+
+class Child1 : Parent() {
+    private var childHeightInCm = 92
+    private var childWeightInKg = 39
+    override fun printVariables() {
+        println("Registered child height is: $childHeightInCm \n Registered child weight is: $childWeightInKg")
+    }
+}
+
+class Child2 : Hair {
+    private val childHairColor = "Brown"
+    override fun hairColor() {
+        println("Registered child hair color is: $childHairColor")
+    }
+}
